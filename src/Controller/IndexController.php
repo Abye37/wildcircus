d87b406c862c578bd893ad8ebcc6675099557da6
Mayte <malapert.mayte@gmail.com>
2020-01-31 00:08:55 +0100

    template index/index for artist

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Artist;
+use App\Repository\ArtistRepository;
 use App\Entity\Home;
 use App\Repository\HomeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,10 +16,12 @@ class IndexController extends AbstractController
     /**
      * @Route("/", name="user_view")
      */
-    public function index(HomeRepository $homeRepository): Response
+    public function index(HomeRepository $homeRepository, ArtistRepository $artistRepository): Response
     {
         return $this->render('index/index.html.twig', [
-            'home' => $homeRepository->findAll()
+            'home' => $homeRepository->findAll(),
+            'artist' => $artistRepository->findAll()
+
         ]);
     }
 }

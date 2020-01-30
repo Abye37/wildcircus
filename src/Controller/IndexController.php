3f3387f3abc129f4ab3db22276c35e88fe7f9732
Mayte <malapert.mayte@gmail.com>
2020-01-31 00:19:36 +0100

    template and image for expose

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Artist;
 use App\Repository\ArtistRepository;
+use App\Entity\Expose;
+use App\Repository\ExposeRepository;
 use App\Entity\Home;
 use App\Repository\HomeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,10 +18,11 @@ class IndexController extends AbstractController
     /**
      * @Route("/", name="user_view")
      */
-    public function index(HomeRepository $homeRepository, ArtistRepository $artistRepository): Response
+    public function index(HomeRepository $homeRepository, ArtistRepository $artistRepository, ExposeRepository $exposeRepository): Response
     {
         return $this->render('index/index.html.twig', [
             'home' => $homeRepository->findAll(),
+            'expose' => $exposeRepository->findAll(),
             'artist' => $artistRepository->findAll()
 
         ]);
